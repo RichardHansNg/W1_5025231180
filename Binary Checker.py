ab30cb65819fae36110091d5a1b4b9347e9e0bdf
RichardHansNg <162283376+RichardHansNg@users.noreply.github.com>
2025-03-03 16:03:33 +0700

    Add files via upload

--- Binary Checker.py
+++ Binary Checker.py
@@ -1,1 +1,19 @@
+def binary_checker(sentence, words):
+    while True:
+        matches = []
+        for word in words:
+            if word in sentence:
+                matches.append(word)
+        if not matches:
+            break
+        longest_match = max(matches, key=len)
+        sentence = sentence.replace(longest_match, "", 1)
+    if sentence == "":
+        print("VALID")
+    else:
+        print("INVALID")
 
+S = {"00", "10", "010", "01001"}
+
+user_input = input()
+binary_checker(user_input, S)
